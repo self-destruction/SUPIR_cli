a8888b9d5cc5f03ae28469b36095181fbbd398ff
Сырчиков Максим <syrchikov_max@mail.ru>
2024-05-06 19:08:15 +0500

    Update test.py

--- test.py
+++ test.py
@@ -81,12 +81,20 @@ for img_pth in os.listdir(args.img_dir):
     captions = []
 
     LQ_ips = Image.open(os.path.join(args.img_dir, img_pth))
-    LQ_img, h0, w0 = PIL2Tensor(LQ_ips, upsacle=args.upscale, min_size=args.min_size)
-    LQ_img = LQ_img.unsqueeze(0).to(SUPIR_device)[:, :3, :, :]
+    input_image = np.array(LQ_ips)
+    input_image = HWC3(input_image)
+    input_image = upscale_image(input_image, args.upscale, unit_resolution=32,
+                                min_size=args.min_size)
+
+    LQ = np.array(input_image) / 255.0
+    LQ *= 255.0
+    LQ = LQ.round().clip(0, 255).astype(np.uint8)
+    LQ = LQ / 255 * 2 - 1
+    LQ = torch.tensor(LQ, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0).to(SUPIR_device)[:, :3, :, :]
 
     # # step 3: Diffusion Process
     print('# start batchify sample')
-    samples = model.batchify_sample(LQ_img, captions, num_steps=args.edm_steps, restoration_scale=args.s_stage1, s_churn=args.s_churn,
+    samples = model.batchify_sample(LQ, captions, num_steps=args.edm_steps, restoration_scale=args.s_stage1, s_churn=args.s_churn,
                                     s_noise=args.s_noise, cfg_scale=args.s_cfg, control_scale=args.s_stage2, seed=args.seed,
                                     num_samples=args.num_samples, p_p=args.a_prompt, n_p=args.n_prompt, color_fix_type=args.color_fix_type,
                                     use_linear_CFG=args.linear_CFG, use_linear_control_scale=args.linear_s_stage2,
