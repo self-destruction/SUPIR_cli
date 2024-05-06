c6cae7602517a4cd28424f7de7a9ea0f0da38fe3
Сырчиков Максим <syrchikov_max@mail.ru>
2024-05-06 22:46:37 +0500

    Update shared.py

--- SUPIR/utils/shared.py
+++ SUPIR/utils/shared.py
@@ -1,8 +1,8 @@
 import types
 
 opts = pipe_encode = types.SimpleNamespace() 
-opts.fp8_storage=False
-opts.opt_channelslast=False
+opts.fp8_storage=True
+opts.opt_channelslast=True
 opts.half_mode = True
-opts.upcast_sampling = False
-opts.fast_load_sd = False
+opts.upcast_sampling = True
+opts.fast_load_sd = True
