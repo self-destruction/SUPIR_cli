f7b97dae9234fba7e434ec843c98a15d0bc2cdac
Сырчиков Максим <syrchikov_max@mail.ru>
2024-05-06 23:24:56 +0500

    Update CKPT_PTH.py

--- CKPT_PTH.py
+++ CKPT_PTH.py
@@ -1,4 +1,4 @@
-LLAVA_CLIP_PATH = '/kaggle/working/SUPIR_kaggle/models/clip-vit-large-patch14-336'
-LLAVA_MODEL_PATH = '/kaggle/working/SUPIR_kaggle/models/llava-v1.5-7b'
-SDXL_CLIP1_PATH = '/kaggle/working/SUPIR_kaggle/models/clip-vit-large-patch14'
-SDXL_CLIP2_CKPT_PTH = '/kaggle/working/SUPIR_kaggle/models/open_clip_pytorch_model.bin'
+LLAVA_CLIP_PATH = 'models/clip-vit-large-patch14-336'
+LLAVA_MODEL_PATH = 'models/llava-v1.5-7b'
+SDXL_CLIP1_PATH = 'models/clip-vit-large-patch14'
+SDXL_CLIP2_CKPT_PTH = 'models/open_clip_pytorch_model.bin'
